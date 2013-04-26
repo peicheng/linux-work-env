02e4ef5b4cf2a2625586c7bb8517004954425fab
Peicheng Liao <peicheng5@gmail.com>
2013-04-27 01:51:51 +0800

    modify settup.sh :add vimrc setting

--- settup.sh
+++ settup.sh
@@ -2,3 +2,6 @@ cp -f /root/.bashrc /root/.bashrc.bak
 rm -f /root/.bashrc
 ln -s /root/linux-work-env/bashrc /root/.bashrc
 source /root/.bashrc
+cp -f /root/.vimrc /root/.vimrc.bak
+rm -f /root/.vimrc
+ln -s /root/linux-work-env/vimrc /root/.vimrc
